cdb228d99a1f0dd76712cbae397f7cf8fd9dc0db
khushmohammad <khushbhaijaan007@gmail.com>
2022-08-12 11:36:33 +0400

    changes

--- application/controllers/FrontEndControllers/Pages.php
+++ application/controllers/FrontEndControllers/Pages.php
@@ -15,14 +15,15 @@ class Pages extends CI_Controller
             'protocol' => 'smtp',
             'smtp_host' => 'smtpout.secureserver.net',
             'smtp_port' => 465,
+            'smtp_timeout' => 5,
             'smtp_user' => 'info@avenuepoultech.com', // change it to yours
             'smtp_pass' => 'e5bY8eR=C&', // change it to yours
             'mailtype' => 'html',
-            //'smtp_auth' => true,
-            'smtp_crypto' => 'tls',
+            'smtp_auth' => true,
+            'smtp_crypto' => 'ssl',
             'charset' => 'UTF-8',
             'wordwrap' => TRUE,
-            
+
         );
         // $this->load->library('email');
         $this->email->initialize($config);
@@ -326,9 +327,8 @@ class Pages extends CI_Controller
         $this->email->message('Testing the email class.');
         $this->email->send();
 
-        
-            $errors = $this->email->print_debugger();
-            print_r($errors);
-       
+
+        $errors = $this->email->print_debugger();
+        print_r($errors);
     }
 }
