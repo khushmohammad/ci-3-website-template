4671c8a78c087fc704510df7531f3e711bbc4e3d
khushmohammad <khushbhaijaan007@gmail.com>
2022-08-12 09:11:15 +0400

    about

--- application/views/FrontEnd/Pages/about.php
+++ application/views/FrontEnd/Pages/about.php
@@ -35,17 +35,14 @@ $this->load->view('FrontEnd/Template/PageHeading', $data);
                     <p>Avenue Poultech is a company that specializes in modern agriculture. We create products and
                         systems to assist farmers all over the world in growing crops while utilizing the best available
                         inputs. We believe that Organic Inputs have the potential to bring humanity’s needs into balance
-                        with our planet’s resources.</p>
-                    <p>Avenue Poultech is a company that specializes in modern agriculture. We create products and
-                        systems to assist farmers all over the world in growing crops while utilizing the best available
-                        inputs. We believe that Organic Inputs have the potential to bring humanity’s needs into balance
-                        with our planet’s resources.</p>
-                    <p>Because of our skilled business ethics, we were able to serve batter to our clients. We have
-                        installed cutting-edge machinery to create world-class products and keep operations running
-                        smoothly. Our entire operation is led by experienced professionals who diligently execute their
-                        assigned tasks within the parameters of productivity, time, and cost. Since our establishment,
-                        we have been meeting the market demands successfully. We have a large number of satisfied
-                        customers as a result of our high-quality products and customer-centric approaches.</p>
+                        with our planet’s resources.
+                    </p>
+                    <p>Avenue Poultech was founded in 2019 by Mr. Yusufddin Dyer and was one of first companies in region to have a 100 percent bio waste utilization system at the time. The company owns egg-laying poultry farms, and the daily poultry litter – bio waste is processed by a biogas plant to produce biogas. The bio slurry discharged from the biogas plant is the primary source of organic fertilizer at our plant. The company’s policy is to provide the best products and never compromise on quality, as evidenced by periodic sample tests of the products at the government level.
+
+                    </p>
+                    <p>Because of our skilled business ethics, we were able to serve batter to our clients. We have installed cutting-edge machinery to create world-class products and keep operations running smoothly. Our entire operation is led by experienced professionals who diligently execute their assigned tasks within the parameters of productivity, time, and cost. Since our establishment, we have been meeting the market demands successfully. We have a large number of satisfied customers as a result of our high-quality products and customer-centric approaches.
+
+                    </p>
                 </div>
             </div>
         </div>
